f7e37344d1f7e225931e595055bffff6352c9c66
Sebastian Giraudo <sebastian.giraudo@globant.com>
2018-01-17 15:55:48 -0800

    displaying a list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,12 +8,13 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
-
   heroes: Hero[] = HEROES;
+
+  selectedHero: Hero;
+
+  onSelectHero(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() {
   }
